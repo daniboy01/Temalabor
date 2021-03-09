634d41b6dde7ce0d29359fa2693ce334f76629db
daniboy01 <pappdaniel012@gmail.com>
2021-03-09 09:28:57 +0100

    Create DAL classes, modify Startup.cs, Add automated migrations

---
+++ Kanban.DAL/KanbanDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Kanban.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanban.DAL
+{
+    public class KanbanDbContext : DbContext
+    {
+        protected KanbanDbContext() { }
+
+        public KanbanDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
---
+++ Kanban.DAL/Models/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanban.DAL.Models
+{
+    public enum State
+    {
+        FÜGGŐBEN,
+        FOLYAMATBAN,
+        KÉSZ,
+        ELHALASZTVA
+    }
+}
