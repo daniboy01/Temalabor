1f2bca410c75d212d1251749e5c3a23304414d97
daniboy01 <pappdaniel012@gmail.com>
2021-03-23 18:02:55 +0100

    enabled cors

--- Kanban.WEB/Startup.cs
+++ Kanban.WEB/Startup.cs
@@ -36,6 +36,12 @@ namespace Kanban.WEB
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+                options.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+            );
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
